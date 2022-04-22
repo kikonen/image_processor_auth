83c6c03752b47ca0a74451e026a0cb72ea7614d7
Kari Ikonen <mr.kari.ikonen@gmail.com>
2022-04-23 01:07:00 +0300

    ADD oauth login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,36 @@ class SessionsController < ApplicationController
     Rails.logger.info "OAUTH"
     Rails.logger.info user_info
 
-    cb_url = "/ui"
-    query = { email: user_info[:info][:email] }.to_query
+
+    api = ApiRequest.new
+    system_token = Token.create_system_token
+    email = user_info[:info][:email]
+    response = api.get(
+      url: "/users/query/by_email",
+      query: { email: email },
+      token: system_token)
+    user = response.content if response.success?
+
+    unless user
+      response = api.post(
+        url: "/users",
+        token: system_token,
+        body: {
+          email: email
+        })
+      user = response.content if response.success?
+    end
+
+    unless user
+      redirect_to login_path
+      return
+    end
+
+    Rails.logger.info user
+
+    # TODO KI hardcoded path; get from "cb"
+    cb_url = "/ui/logged_in"
+    query = { id: user[:id] }.to_query
     redirect_to "#{cb_url}?#{query}", allow_other_host: true
   end
 end
